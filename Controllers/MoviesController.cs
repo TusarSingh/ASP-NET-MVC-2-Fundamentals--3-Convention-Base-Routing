14587956bfdf1bd81ba95e6fb11acc384458071e
Tusar Singh <rts0941@gmail.com>
2023-10-10 12:42:43 +0530

    Convention Base Routing

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Tarun" };
+
+            return View(movie);
+        }
+
+
+        public ActionResult ByReleaseDate(int? Year,int? Month)
+        {
+            return Content(Year + "/" + Month);
+        }
+
+    }
+}
